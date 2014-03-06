a9bf9233be74403363030d7a10d9987eafbb494f
SchreiberG <greg_schreiber@hotmail.com>
2014-03-05 22:14:52 -0600

    Got the webView working and searching recipe site.

--- MealPicker/mpRecipeViewController.h
+++ MealPicker/mpRecipeViewController.h
@@ -10,4 +10,6 @@
 
 @interface mpRecipeViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
 @end
--- MealPicker/mpRecipeViewController.m
+++ MealPicker/mpRecipeViewController.m
@@ -18,6 +18,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+   
+    // The last word in this string is the search word and needs to be the dine in
+    // food description - build the string using a variable from the dine in array
+    NSString *urlAddress = @"http://m.allrecipes.com/search/recipes?wt=nachos";
+    //Create a URL object.
+    NSURL *url = [NSURL URLWithString:urlAddress];
+    //URL Requst Object
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    //Load the request in the UIWebView.
+    [_webView loadRequest:requestObj];
 }
 
 - (void)didReceiveMemoryWarning
